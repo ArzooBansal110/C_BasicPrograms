bcbca350142ef14f1703d01c420d039ffecb6661
anmolbansal08 <mailmeanmolbansal@gmail.com>
2024-11-28 22:31:59 +0530

    integers.c
    Integers

---
+++ Basic Declarations and Expressions/integers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int p, q, r, s;
+    printf("q:  \nr:  \ns:  \n  p:  \n");
+    scanf("%d %d %d %d", & q, &r, &s, &p);
+
+    if (q > r && s > p)
+    {
+        if (r + s > q + p)
+        {
+            printf("Correct Value");
+        }
+        else
+        {
+            printf("Wrong Value");
+        }
+    }
+    return 0;
+}
